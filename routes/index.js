6d115852cb7294d57a348b70a51d13ef5f80ed8f
David Buettner <dbuettner@hawk.iit.edu>
2018-06-29 19:58:31 -0500

    Refractored code.  Change naming of variables. Added more comments. Plus eslint and stylelint used

--- routes/index.js
+++ routes/index.js
@@ -5,54 +5,48 @@ const express = require('express');
 const router = express.Router();
 const reqprom = require('request-promise-native');
 
-const magall = -1;
-const weekall = -1
+const mag_all = -1;
+const week_all = -1;
 
-let magsize = [];  // Define and intialize the tremor magnitude size
-let maglabel = [];
-let weekcnt = [];
-let weeklabel = [];
+let mag_count = [];  // Count column for the magnitude table; tally of magnitudes of the various size
+let mag_label = []; // Label column for the magnitude table
+let week_count = [];  // Count column for weekly table; tally of the number of tremors that week
+let week_label = []; // Label column for weekly table and is first day of the week count
+
+// define the JSON object has the data provided to the tremors.pug file to expand the HTML
 let renderinfo = {
-      magsize: magsize,
-      maglabel: maglabel,
-      date: Date(),
-      weekcnt: weekcnt,
-      weeklabel: weeklabel
+  mag_count: mag_count,
+  mag_label: mag_label,
+  date: Date(),
+  week_count: week_count,
+  week_label: week_label
 };
-
-
 
 /* GET home page. */
 router.get('/', function(req, res) {
-
-  fetchtremors(res, magall, weekall);
-
+  // Call the fetch tremors function with the default values to print out all magnitudes in all weeks
+  fetchtremors(res, mag_all, week_all);
 });
 
+/* POST web page */
 router.post('/', function(req, res) {
-  console.log("Got Here!!");
-  console.log(req.body.magnitude);
-  console.log(req.body.week);
-  fetchtremors(res,Number(req.body.magnitude), Number(req.body.week));
-
-  // fetchtremors(res, magall, weekall);
-
-  // res.send(req.body);
-  // res.send(JSON.stringify(req.body.form));
+  // Call the fetch tremors function with the values that are in the magnitude and week selection drop downs
+  fetchtremors(res, Number(req.body.magnitude), Number(req.body.week));
 });
 
-
+/* The key function used by both the GET & POST methods to get data and send it to tremors.pug to render. */
 function fetchtremors (res, magselect, weekselect) {
   // Set up the header parameters for the usgs.gov request for earthquake data
   const options = {
-    // The URL to get all records around Kilauea since May 3rd to the end of class, July 1st
-    uri: 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-05-03&endtime=2018-07-01&latitude=19.40&longitude=-155.27&maxradiuskm=80',
+    // The URL to get all records around Kilauea since May 3rd to July 4th which is first complete week after end of class
+    uri: 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-05-03&endtime=2018-07-04&latitude=19.40&longitude=-155.27&maxradiuskm=80',
     headers: {
       'User-Agent': 'Request-Promise'
     },
     json: true
   };
 
+  /* go fetch USGS API data with request-promise */
   reqprom(options)
     .then(function(response) {
       const tremors = [];  // Array containing the list of properties for one earthquake/record
@@ -60,65 +54,73 @@ function fetchtremors (res, magselect, weekselect) {
 
       // Get weekly counts from beginning date
       const sdate = Date.parse('May 3, 2018');
-      const sdate2 = new Date(sdate);
-      const wmilli = (7*24*60*60*1000); // Number of milliseconds in a week
+      const millisec2week = (7*24*60*60*1000); // Number of milliseconds in a week
       let weeknummax = 0;
-      magsize.length = 0;
-      // maglabel.length = 0; // Keep the labels
-      weekcnt.length = 0;
-      // weeklabel.length = 0; // Keep the labels
+      // Truncate the mag_count and week_count arrays otherwise prior queries cumulate
+      mag_count.length = 0;
+      week_count.length = 0;
       
-
       // Loop through all the JSON records returned by the USGS API
       for ( let i = 0; i < response.features.length; i++ ) {
-        // Declare the needed variables
+
+        // Declare variables to point to the USGS API properties and geometrys part of the returned JSON object
         const getprop = response.features[i].properties;
         const getgeo = response.features[i].geometry;
-        let title, mag, time, lngtd, latt, magtrunc;
+        
         // Get the title first and use to filter out the earthquakes not associated with the Volcano
-        // const title = response.features[i].properties.title;
-        title = getprop.title;
+        const title = getprop.title;
         if ( title.match('Volcano')) {
-          mag = getprop.mag;
-          time = new Date(getprop.time);
-          lngtd = response.features[i].geometry.coordinates[0];
-          latt = response.features[i].geometry.coordinates[1];
+          // Declare and assign the other required fields of magnitude, time of tremore, longatude and lattitued
+          const mag = getprop.mag;
+          const magtrunc = Math.trunc(mag); // Convert magnitude from decimal to integer value
+          const time = new Date(getprop.time);
+          const lngtd = getgeo.coordinates[0];
+          const latt = getgeo.coordinates[1];
+
+          // Convert the time of tremor to the week number from May 3rd.
+          // Subtract the tremor time from the May 3rd time and then divide to go from milliseconds to weeks
+          const week_index = Math.trunc( ( Date.parse(time) - sdate) / millisec2week );
+
+          // See if magnitude and week parameters are to be included. With GET method, all will fall through
+          // With POST method, some to many may be filtered out
+          if ( (magselect !== mag_all && magselect !== magtrunc) || (weekselect !== week_all && weekselect !== week_index) ) {
+            continue; // Go to for loop
+          }
+
           // Add the new earthquake properties to the tremor array
           tremors.push({num: recordid++, title: title, mag: mag, time: time, lngtd: lngtd, latt: latt});
-          // Make table for range
-          magtrunc = Math.trunc(mag);
-          let wknm = Math.trunc( ( Date.parse(time) - sdate) / wmilli );
-          if ( (magselect !== magall && magselect !== magtrunc) || (weekselect !== weekall && weekselect !== wknm) ) {
-            recordid--;
-            continue;
-          }
+
+          // Convert the time of tremor to the week number from May 3rd.
+          // Subtract the tremor time from the May 3rd time and then divide to go from milliseconds to weeks
+          // let week_index = Math.trunc( ( Date.parse(time) - sdate) / millisec2week );
+          
           if ( magtrunc >= 5.0 ) {
-            magsize[5] = magsize[5] ? magsize[5] + 1 : 1;
-            maglabel[5] = 'greater than 5.00';
+            mag_count[5] = mag_count[5] ? mag_count[5] + 1 : 1;
+            mag_label[5] = 'greater than 5.00';
           } else {
-            magsize[magtrunc] = magsize[magtrunc] ? magsize[magtrunc] + 1 : 1;
-            maglabel[magtrunc] = `${ magtrunc } - ${ magtrunc }.99`; 
+            mag_count[magtrunc] = mag_count[magtrunc] ? mag_count[magtrunc] + 1 : 1; // If variable undefined, set to 1
+            mag_label[magtrunc] = `${ magtrunc } - ${ magtrunc }.99`; 
           }
-          let swkdt = new Date( ( wknm * wmilli ) + sdate );
-          weekcnt[wknm] = weekcnt[wknm] ? ++weekcnt[wknm] : 1;
-          weeklabel[wknm] = swkdt.toDateString().replace(/..../, '');
-          weeknummax = weeknummax < wknm ? wknm : weeknummax;
+          let swkdt = new Date( ( week_index * millisec2week ) + sdate );
+          week_count[week_index] = week_count[week_index] ? ++week_count[week_index] : 1; // If variable undefined, set to 1
+          week_label[week_index] = swkdt.toDateString().replace(/..../, ''); // Remove the 3 character day of week plus space
+          weeknummax = weeknummax < week_index ? week_index : weeknummax; // The weekly table increase and need the max value
         }
       }
-      magsize[6] = recordid; // tremors.length;
-      maglabel[6] = 'TOTAL';
-      weekcnt[ weeknummax + 1] = recordid; // tremors.length;
-      weeklabel[ weeknummax + 1] = "TOTAL";
-      renderinfo.weekcnt = weekcnt;
-      renderinfo.weeklabel = weeklabel;
+      mag_count[6] = recordid; 
+      mag_label[6] = 'TOTAL';
+      week_count[ weeknummax + 1] = recordid; 
+      week_label[ weeknummax + 1] = "TOTAL";
+      renderinfo.week_count = week_count;
+      renderinfo.week_label = week_label;
 
       res.render('tremors', renderinfo );
 
-  })
-  .catch(function(err) {
+    })
+    .catch(function(err) {
     // The API failed and report error
-    console.log(err);
-  });
+      console.log(err);
+    });
 }
 
 module.exports = router;
